4060a2e1f90ad76974d80555a6a90c9893d4e4fc
Ben Green <benjclaytongreen@gmail.com>
2022-11-18 19:00:55 +0000

    Tiles have pipe openings controled by north, east, south, west properties in the data

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Tile from './Tile';
 
-type TileData = {
+export type TileData = {
   x: number;
   y: number;
+  openings: PipeOpenings;
+}
+
+/** North, east, south, west 
+ * and whether they have a pipe opening or not. */
+export type PipeOpenings = {
+  n?: boolean;
+  e?: boolean;
+  s?: boolean;
+  w?: boolean;
 }
 
 const generateTiles = (width: number, height: number): TileData[] => {
   const tiles: TileData[] = [];
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
-      tiles.push({x, y});
+      tiles.push({
+        x, 
+        y, 
+        openings: {n: true, s: true}
+      });
     }
   }
   return tiles;
 }
 
-const initialTiles: TileData[] = generateTiles(4,3);
+const height = 3;
+const width = 4;
+
+const initialTiles: TileData[] = generateTiles(width,height);
 
 function App() {
   
@@ -25,10 +42,21 @@ function App() {
 
   const tiles: JSX.Element[] = [];
   
-  for (let y = 0; y < tileData.length/4; y++) {
+  for (let y = 0; y < height; y++) {
     const row: JSX.Element[] = [];
-    for (let x = 0; x < 4; x++) {
-      row.push(<Tile/>);
+    for (let x = 0; x < width; x++) {
+      const tile = tileData.find(
+        tile => tile.x == x && tile.y == y
+      );
+      
+      if (tile) {
+        row.push(
+          <Tile 
+            x={tile.x} 
+            y={tile.y}
+            openings={tile.openings}/>
+        );
+      }
     }
     tiles.push(<div className="row">{row}</div>)
   }
--- src/Tile.tsx
+++ src/Tile.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import { PipeOpenings, TileData } from './App';
 import './Tile.css';
 
-function Tile() {
+function Tile(tile: TileData) {
+
+  const renderPipe = (openings: PipeOpenings) => {
+    if (!(openings.n || openings.e || openings.s || openings.w)) {
+      return;
+    }
+
+    const topOpening = openings.n && <div className="pipe north"></div>;
+    const rightOpening = openings.e && <div className="pipe east"></div>;
+    const bottomOpening = openings.s && <div className="pipe south"></div>;
+    const leftOpening = openings.w && <div className="pipe west"></div>;
+
+    return (
+      <div className='pipe-container'>
+        <div className="pipe center"></div>
+        {topOpening}
+        {rightOpening}
+        {bottomOpening}
+        {leftOpening}
+      </div>
+    );
+  }
+
   return (
     <div className="tile">
-      <img src={logo} className="App-logo" alt="logo" />
+      {renderPipe(tile.openings)}
     </div>
   );
 }
